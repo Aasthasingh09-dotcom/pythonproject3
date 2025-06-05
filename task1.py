9139a53e4130a79eb9980210a8cc98cd7abe150b
Aasthasingh09-dotcom <aasthasinghrajput09@gmail.com>
2025-06-05 21:16:04 +0530

    Add files via upload
    assignment 1 both the task uploaded

---
+++ task1.py
@@ -0,0 +1,12 @@
+a=input('enter first number')
+b=input('enter second number')
+a = int(a)
+b = int(b)
+c = a + b
+print('ADDITION: ', c)
+d = a - b
+print('SUBSTRACTION: ' , d)
+e = a * b
+print('MULTIPLICATION: ', e)
+f = a / b
+print('DIVISION: ', f )
---
+++ task2.py
@@ -0,0 +1,4 @@
+g=input("enter your first name")
+h=input("enter your last name")
+i=g + h
+print('HELLO!' ,i + ' welcome ' + 'to ' + 'python' + ' programming!' )
